1deedac3e81b4ca03967bf9adaa2f2a4a47d5bd9
1160300919 <1034538932@qq.com>
2018-04-21 09:35:09 +0800

    this is the solution to module4

---
+++ module4/script.js
@@ -0,0 +1,13 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i=0;i<names.length;i++) {
+     var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter=='j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
